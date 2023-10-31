f58a9bbae4d7acb4dc8296054904543a6a9e6979
Carlos <bowyercarlos@gmail.com>
2023-10-30 22:04:00 -0400

    looping functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+const names = ["Nick","Yetla","Benji"]
+
+const event = "Halloween"
+
+const writeCards = (arr,event) => {
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+        
+           const greetingMessage = `Thank you, ${names[i]}, for coming to my ${event} party!`
+           messages.push(greetingMessage)
+        
+    }
+    return messages
+
+}
+console.log(writeCards(["Nick", "Yetla", "Benji"], "Halloween"))
+
+
+let countDown = 10;
+while (countDown >=0){
+    console.log(countDown--)
+}
